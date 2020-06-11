1779c72d3e082bdfba82d0801ca99564438ae0b8
mmbukane-shy6 <sheilaegeidza@gmail.com>
2020-06-11 12:47:40 +0300

    added new item component

--- Angular8ClientCrud/src/app/components/add-tutorial/add-tutorial.component.ts
+++ Angular8ClientCrud/src/app/components/add-tutorial/add-tutorial.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TutorialService } from 'src/app/services/tutorial.service';
 
 @Component({
   selector: 'app-add-tutorial',
@@ -6,10 +7,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-tutorial.component.css']
 })
 export class AddTutorialComponent implements OnInit {
-
-  constructor() { }
+  tutorial = {
+    title: '',
+    description: '',
+    published: false
+  };
+  submitted = false;
+  constructor(private tutorialService: TutorialService) { }
 
   ngOnInit() {
   }
 
-}
+  saveTutorial() {
+    const data = {
+      title: this.tutorial.title,
+      description: this.tutorial.description
+    };
+
+    this.tutorialService.create(data)
+      .subscribe(
+        response => {
+          console.log(response);
+          this.submitted = true;
+        },
+        error => {
+          console.log(error);
+        });
+      }
+    newTutorial() {
+      this.submitted = false;
+      this.tutorial = {
+        title: '',
+        description: '',
+        published: false
+      };
+    }
+  }
