dc0e00c910f3ad4fb250ce5d18762415cb493aac
Buttonupd <Dankariuki0101@gmail.com>
2020-06-12 11:26:45 +0300

    Integrated an Angular app which         consumes a Django Api inside the Django dir

--- Angular8ClientCrud/src/app/services/tutorial.service.ts
+++ Angular8ClientCrud/src/app/services/tutorial.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-const baseUrl = 'http://localhost:8080/api/tutorials';
+const baseUrl = 'http://127.0.0.1:8000/api/tutorial';
 
 
 @Injectable({
